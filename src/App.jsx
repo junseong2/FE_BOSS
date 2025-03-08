63ddeb7acc71f59244cb90e3d58ef3c4a0a577cb
JJH037 <tmflfkd07@gmail.com>
2025-03-08 19:59:29 +0900

    feat:App.css분리 및 팝업카트페이지 실시간적용 및 페이지 통합

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { CartProvider } from './context/CartContext'; // CartProvider import
+
 import './App.css';
+import './layout.css';
 
 import Top from './components/layout/Top';
 import MenuBar from './MenuBar';
@@ -14,7 +17,15 @@ import MyPage from './pages/MyPage';
 import EventPage from './pages/EventPage';
 import SignUp from './pages/SignUp';
 import CategoryPage from './pages/CategoryPage';
-import SearchPage from './pages/SearchPage'; 
+import SearchPage from './pages/SearchPage';
+import ProductListPage from './pages/ProductListPage.jsx';
+import SellerPage from './pages/sellerDashboard/SellerPage.jsx';
+import SellerDashboardPage from './pages/sellerDashboard/SellerDashboardPage.jsx';
+import SellerProductPage from './pages/sellerDashboard/SellerProductPage.jsx';
+import SellerOrderPage from './pages/sellerDashboard/SellerOrderPage.jsx';
+import SellerInventoryPage from './pages/sellerDashboard/SellerInventoryPage.jsx';
+import SellerSalesPage from './pages/sellerDashboard/SellerSalesPage.jsx';
+import SellerPaymentPage from './pages/sellerDashboard/SellerPaymentPage.jsx';
 
 function App() {
   const [memberData, setMemberData] = useState(() => {
@@ -33,32 +44,47 @@ function App() {
 
   // 현재 경로 가져오기
   const location = useLocation();
-  const isAdminPage = location.pathname.startsWith('/admin', '/seller'); // `/admin`으로 시작하면 true
+  const isAdminPage =
+    location.pathname.startsWith('/seller') || location.pathname.startsWith('/admin'); // `/admin`으로 시작하면 true
 
   return (
-    <div className='flex'>
-      {/* 관리자 페이지가 아닐 때만 MenuBar와 Top을 렌더링 */}
-      {!isAdminPage && <MenuBar />}
-      <div className={`flex-1 ${!isAdminPage ? 'ml-60' : ''}`}>
-        {!isAdminPage && <Top />}
-        <main className='main page'>
-          <Routes>
-            {/* 일반 페이지 */}
-            <Route path='/' element={<HomePage memberData={memberData} onAdd={handleAdd} />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/contact' element={<ContactPage />} />
-            <Route path='/event' element={<EventPage />} />
-            <Route path='/camera' element={<CameraCapturePage onAdd={handleAdd} />} />
-            <Route path='/signin' element={<SignIn />} />
-            <Route path='/mypage' element={<MyPage />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/cart' element={<CartPage />} />
-            <Route path="/category/:categoryId" element={<CategoryPage />} />
-            <Route path="/search" element={<SearchPage />} />
-          </Routes>
-        </main>
+    <CartProvider>
+      {' '}
+      {/* CartContext로 감싸기 */}
+      <div className='flex'>
+        {/* 관리자 페이지가 아닐 때만 MenuBar와 Top을 렌더링 */}
+        {!isAdminPage && <MenuBar />}
+        <div className={`flex-1 ${!isAdminPage ? 'ml-60' : ''}`}>
+          {!isAdminPage && <Top />}
+          <main className='main page'>
+            <Routes>
+              {/* 일반 페이지 */}
+              <Route path='/' element={<HomePage memberData={memberData} onAdd={handleAdd} />} />
+              <Route path='/about' element={<AboutPage />} />
+              <Route path='/contact' element={<ContactPage />} />
+              <Route path='/event' element={<EventPage />} />
+              <Route path='/camera' element={<CameraCapturePage onAdd={handleAdd} />} />
+              <Route path='/signin' element={<SignIn />} />
+              <Route path='/mypage' element={<MyPage />} />
+              <Route path='/signup' element={<SignUp />} />
+              <Route path='/cart' element={<CartPage />} />
+              <Route path='/category/:categoryId' element={<CategoryPage />} />
+              <Route path='/search' element={<SearchPage />} />
+              <Route path='/productlist' element={<ProductListPage />} />
+
+              <Route path='/seller' element={<SellerPage />}>
+                <Route index path='dashboard' element={<SellerDashboardPage />} />
+                <Route path='product' element={<SellerProductPage />} />
+                <Route path='order' element={<SellerOrderPage />} />
+                <Route path='inventory' element={<SellerInventoryPage />} />
+                <Route path='sales' element={<SellerSalesPage />} />
+                <Route path='payment' element={<SellerPaymentPage />} />
+              </Route>
+            </Routes>
+          </main>
+        </div>
       </div>
-    </div>
+    </CartProvider>
   );
 }
 
