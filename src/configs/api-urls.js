fd9344eea7f6af820d517d45f0e21f9d5867a48e
Kim Young Wan <qodna25@gmail.com>
2025-03-10 16:59:07 +0900

    feat: 판매자 대시보드 상품 조회, 수정, 삭제 api 연동 및 페이지 레이아웃 설정

---
+++ src/configs/api-urls.js
@@ -0,0 +1,21 @@
+const BASE_URL = 'http://localhost:5000';
+
+export const apiRoutes = {
+  // 판매자
+  seller: {
+    // 상품 관리
+    products: {
+      getAll: (page, size) => BASE_URL + `/seller/products?page=${page}&size=${size}`,
+      search: (page, size, search) =>
+        BASE_URL + `/seller/products/search?search=${search}&page=${page}&size=${size}`,
+      insert: () => BASE_URL + `/seller/products`,
+      delete: () => BASE_URL + `/seller/products`,
+      update: (productId) => BASE_URL + `/seller/products/${productId}`,
+    },
+    // 재고 관리
+    inventories: {
+      getAll: (page, size) => BASE_URL + `/seller/inventories?page=${page}&size=${size}`,
+      update: () => BASE_URL + `/seller/inventories`,
+    },
+  },
+};
