3364a0236ce8bd3cb4abe0377176b188a351a40b
youngwan2 <qodna25@gmail.com>
2025-04-20 18:14:55 +0900

    feat: pwa 적용

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,52 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      injectRegister: 'auto',
+      registerType: 'autoUpdate',
+      includeAssets: ['**/*'],
+      manifest: {
+        name: 'Vite PWA Project',
+        short_name: 'Vite PWA Project',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg,ico,json}'],
+      },
+    }),
+  ],
   server: {
     port: 5173,
     historyApiFallback: true,
-    
   },
-  
 });
