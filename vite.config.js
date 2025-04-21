14b07d03fcfa7dde38412a9f72fddea4b1707b06
= <spdlvka147@gmail.com>
2025-04-21 12:54:34 +0900

    ssibbal

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
